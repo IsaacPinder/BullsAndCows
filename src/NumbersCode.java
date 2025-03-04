188ec571b1581f90407a1d7729123500d6126883
Divine Obi CS2022 <divine.obi.2022@uni.strath.ac.uk>
2025-03-04 17:28:38 +0000

    Edit NumbersCode.java

--- src/NumbersCode.java
+++ src/NumbersCode.java
@@ -1,9 +1,25 @@
 import java.util.*  ;
+
 public class NumbersCode extends SecretCode {
-
-        NumbersCode(){
-        // deciphered code = generated code
-                decipheredCode = "1234";
+        public NumbersCode() {
+                decipheredCode = codeGenerator();
         }
+        private String codeGenerator() {
+                // this is responsible in creating the 4digit code and the numbers are between 0-9
+                List<Integer> num = new ArrayList<>();
+                for (int i = 0; i < 10; i++) {
+                        num.add(i);
+                }
+                // this is used to randomly reorder the elements in the list
+                Collections.shuffle(num);
 
+                // this process builds the 4-digit code sequentially without the overhead of creating a new string at every step.
+                StringBuilder code = new StringBuilder();
+                for (int i = 0; i < 4; i++) {
+                        code.append(num.get(i));
+                }
+                return code.toString();
         }
+}
+
+
