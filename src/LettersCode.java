8854e916dc769400296461e4f188a82225bcb88d
Rayane Touileb SE2023 <rayane.touileb.2023@uni.strath.ac.uk>
2025-03-06 12:21:36 +0000

    LettersCode remodified to fit new changes

--- src/LettersCode.java
+++ src/LettersCode.java
@@ -1,18 +1,15 @@
-import java.util.ArrayList;
-import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
 
 public class LettersCode extends SecretCode {
-//lc stands for "LettersCode", but you can name it anything and we using it cuz It calls the constructor LettersCode("issac.txt"), which reads the file and prints a random word.
-     //   LettersCode LettersCode = new LettersCode("lettercode.txt"); // or new LettersCode("lettercode.txt");
+    //lc stands for "LettersCode", but you can name it anything and we using it cuz It calls the constructor LettersCode("lettercode.txt"), which reads the file and prints a random word.
+    //   LettersCode LettersCode = new LettersCode("lettercode.txt"); // or new LettersCode("lettercode.txt");
     public LettersCode(String filename) {
         ArrayList<String> words = readFile(filename); // Read file into list
         if (!words.isEmpty()) {
             String decipheredCode = getRandomWord(words);
-            System.out.println("Deciphered Code: " + decipheredCode);
-        } else {
-            System.out.println("Error: File is empty or not found."); //
         }
+
+
     }
 
     private ArrayList<String> readFile(String filename) {
@@ -20,7 +17,7 @@ public class LettersCode extends SecretCode {
         try {
             Scanner scanner = new Scanner(new java.io.File(filename)); // Read file
             while (scanner.hasNextLine()) {
-                words.add(scanner.nextLine().toLowerCase()); // Store words in LOWERCASE ISSSAC
+                words.add(scanner.nextLine().toLowerCase()); // Store words in LOWERCASE
             }
             scanner.close();
         } catch (Exception e) { // Handle errors when file not found
