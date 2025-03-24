3a8ba245094a08ff31c8472d60d17bd6c39753d1
Rayane Touileb SE2023 <rayane.touileb.2023@uni.strath.ac.uk>
2025-03-24 12:17:53 +0000

    sort

--- src/Players.java
+++ src/Players.java
@@ -28,6 +28,30 @@ public class Players {
         System.out.println("Player does not exist");
         return null;
     }
-
+    // sort array list by codesDeciphered
+    void sortPlayers() {
+        int size = allPlayers.size();
+        int i;
+        int j;
+        Player temp;
+        boolean swapped;
+        //using bubble sort for the structure
+        for (i = 0; i < size - 1; i++) {
+            swapped = false;
+            for (j = 0; j < size - i - 1; j++) {
+                //and using the if statement
+                if (allPlayers.get(j).getCodesDeciphered() < allPlayers.get(j + 1).getCodesDeciphered()) {
+                    // Swap arr[j] and arr[j+1] just like regular bubble just with the allplayers
+                    temp = allPlayers.get(j);
+                    allPlayers.set(j, allPlayers.get(j + 1));
+                    allPlayers.set(j + 1, temp);
+                    swapped = true;
+                }
+            }
+            //this is just so if no 2 elements are swapped we can end incase we have a pre sorted list or we at the end
+            if (swapped == false)
+                break;
+         }
+    }
 
 }
